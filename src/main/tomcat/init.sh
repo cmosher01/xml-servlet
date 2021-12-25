2793f197c2429e8746cc73ff908d2159ebb82e39
Christopher A. Mosher <cmosher01@gmail.com>
2021-12-25 18:34:47 -0500

    add maven pub

--- src/main/tomcat/init.sh
+++ src/main/tomcat/init.sh
@@ -1,9 +1,20 @@
-#!/bin/sh
+#!/bin/sh -x
+
+if [ -z "$1" ]
+then
+  echo "usage: $0 dir-path" >&2
+  exit 1
+fi
 
 cd /usr/local/tomcat/webapps
-mkdir ROOT
-cd ROOT
-jar xvf /root/ROOT.war
-ln -s /srv test
+if [ -e ROOT ] ; then
+  echo "ROOT webapp already exists, will not overwrite"
+else
+  mkdir ROOT
+  cd ROOT
+  jar xvf /root/war/*.war
+  ln -v -s $1
+fi
 cd ../..
+
 exec bin/catalina.sh run
